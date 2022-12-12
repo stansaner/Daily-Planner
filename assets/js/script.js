fda771b5e31cb90af71a8a3fa7e8992cf6d45bac
Stan Saner <stan.saner@hotmail.co.uk>
2022-12-12 22:04:46 +0000

    Adding basic display of hourly slots

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
 var currentDay = moment().format("dddd, MMMM Do");
 
+var output = $(".container");
+
+// for the event listener on the buttons
+// Using custom data to distinguish which button was pressed
+// so we save the text to the appropriate object property
+var saveBtn = $('.saveBtn');
+
 // We need to know present hour to colour the background
 // of hour slots for the past, present an future ones.
 var presentHour = moment().format("H");
@@ -27,7 +34,7 @@ currentDayEl.text(currentDay);
 // object of key-value pairs, one pair for each hour slot,
 // key being the hour, value the text of to-dos
 
-var timeslots ={
+var timeSlots = {
     09: '',
     10: '',
     11: '',
@@ -38,3 +45,37 @@ var timeslots ={
     16: '',
     17: ''
 }
+
+
+function getHourSlots() {
+    // This returns either items in the array or an empty object 
+    // if there are no items in the object yet
+    return JSON.parse(localStorage.getItem('dayPlans')) || {};
+}
+
+function saveHourSlots(arr) {
+    localStorage.setItem('dayPlans', JSON.stringify(timeSlots));
+}
+
+
+function displayHourSlots() {
+    var itemsArray = getHourSlots();
+
+    // clear the area before displaying data
+    output.html('');
+
+    $.each(timeSlots, function(key, value){
+        var hour = key;
+        var todos = value;
+        output.append(`
+            <div class="row">
+                <div class="hour">${hour}</div>
+                <textarea id="textarea" rows="3" cols="80" class="past"></textarea>
+                <button class="saveBtn" data-hour="${hour}"><i class="fas fa-save"></i>
+                </button>
+            </div>
+        `);
+     })
+}
+
+displayHourSlots(); 

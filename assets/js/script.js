47db5b7b792b5eb7e16fb30c7f49c211e0c265a1
Stan Saner <stan.saner@hotmail.co.uk>
2023-01-02 14:03:54 +0000

    Major rewrite to fix bug when loading with no data

--- assets/js/script.js
+++ assets/js/script.js
@@ -69,18 +69,42 @@ function saveHourSlots(obj) {
 
 
 function displayHourSlots() {
-    // load hour slots from localStorage
-    var timeSlots = getHourSlots();
-    // var itemsArray = getHourSlots();
-    
+
     // clear the area before displaying data
     output.html('');
     var timeClass ='past';
 
-    var timeSlots = {
-        9: ''
-    }
+    // Each time block is a flex box with flex-direction row:
+    // - row (using the .row class)
+    // - with hour displayed on the left (using the .hour class)
+    // - text area in the centre
+    // blue button on the right to save the text to local storage
+    // Use .past, .present and .future styling classes
+    // to style those blocks relative to the current time
 
+    // We have only a static display for one (present) day
+    // for a fixed number of time slots between 9:00 and 17:00 hour
+    // Taking a lazy approach and hard coding the time slots in an 
+    // object of key-value pairs, one pair for each hour slot,
+    // key being the hour, value the text of to-dos
+
+    // Need to define this second time inside this function
+    // without it timeSlots can be undefined from different
+    // code paths entering this function
+    
+    var timeSlots = {
+        9: '',
+        10: '',
+        11: '',
+        12: '',
+        13: '',
+        14: '',
+        15: '',
+        16: '',
+        17: ''
+}
+    // First display an empty day plan, then load the data
+    // from local storage and update corresponding slots.
     // Loop over the hour slots and assign corresponding 
     // class to style each slot depending on it being
     // in the past, present of future relative to current time
@@ -98,24 +122,58 @@ function displayHourSlots() {
         }
 
         // console.log("timeClass is: " + timeClass);
-        
+
+        // Use custom data textIndex and hourIndex
+        // to identify which button in which
+        // row was pressed. This is important for later to store
+        // the new data in the right property inside the 
+        // timeSlots object
+
         output.append(`
             <div class="row">
                 <div class="hour">${hour}</div>
-                <textarea id="textarea" rows="3" cols="80" class=${timeClass}>${value}</textarea>
+                <textarea id="textarea" data-textIndex="${hour}" rows="3" cols="80" class=${timeClass}>${value}</textarea>
                 <button class="saveBtn" data-hourIndex="${hour}"><i class="fas fa-save"></i>
                 </button>
             </div>
         `);
      })
+
+
+    // load hour slots from localStorage
+    var timeSlots = getHourSlots();
+
+    // Now copy what was loaded from local storage
+    // to the corresponding text areas of each time
+    // slot on the web page
+    
+    if (timeSlots) {
+        // console.log("timeSlots are not empty");
+
+        $.each(timeSlots, function(key, value){
+            var hour = Number(key);
+            
+            // console.log($(this));
+            // console.log($(this).val(value));
+
+            // Select the corresponding text area in the output
+            // based on the custom data 'textIndex'
+            $(`[data-textIndex = "${hour}"]`).val(value);
+        });
+    } else {
+        // console.log("timeSlots are empty");
+    }
 }
 
-allRows.on('click', 'button', function(){
+// Event listener for buttons.
+// Relying on event bubbling up to the 'output' container
+// and identifying which button was pressed 
+output.on('click', 'button', function(){
 
-    var btn =$(this);
+    var btn = $(this);
     var buttonIndex = btn.data('hourIndex');
 
-    console.log('clicked');
+    // console.log('clicked');
 
     toDos = $(this).siblings('#textarea').val();
     hour = $(this).siblings('.hour').text(); 
